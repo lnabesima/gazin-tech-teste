82c39a0e6a35bf4d8703ca8257576af77ef9bc0c
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-02-02 13:21:25 -0300

    feat: add LevelsRepository implementation for levels management
    Introduce a repository class to handle CRUD operations for the Levels
    model using Prisma. Includes methods for retrieving all levels, finding
    by ID, creation, updates, and deletion with error handling for unique
    constraint violations.

---
+++ backend/src/infrastructure/repositories/levels.repository.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@nestjs/common';
+import { Levels } from 'src/domain/models/levels.model';
+import { ILevelsRepository } from '../../domain/repositories/levels.interface';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class LevelsRepository implements ILevelsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAll(): Promise<Levels[]> {
+    return await this.prisma.levels.findMany();
+  }
+
+  async getById(id: number): Promise<Levels | null> {
+    return await this.prisma.levels.findUnique({
+      where: { id }
+    })
+  }
+
+  async create(levelData: Prisma.LevelsCreateInput): Promise<Levels> {
+    try {
+      return await this.prisma.levels.create({
+        data: levelData
+      })
+    } catch (error) {
+      if(error instanceof Prisma.PrismaClientKnownRequestError){
+        if (error.code === 'P2002'){
+          throw new Error('A record with the given unique fields already exists.');
+        }
+      }
+      //TODO: write specific error handlers
+      throw error;
+    }
+  }
+
+  async update(id: number, levelData: Prisma.LevelsUpdateInput): Promise<Levels | null> {
+    const level = await this.prisma.levels.findUnique({
+      where: { id }
+    })
+
+    if (!level){
+      return null
+    }
+
+    return await this.prisma.levels.update({
+      where: { id },
+      data: {
+        ...levelData,
+      }
+    })
+  }
+
+  async delete(id: number): Promise<number | null> {
+    try {
+      await this.prisma.levels.delete({
+        where: { id },
+      });
+      return id;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError)
+        if (error.code === 'P2025') {
+        return null;
+      }
+      throw error;
+    }
+  }
+}
