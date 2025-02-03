86b2a57eae2d63fea3dd96811ebfa9102a14f567
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-02-03 08:11:49 -0300

    refactor(levels): streamline error handling and update types
    Replaced custom error handling logic with a shared `handleError` utility
     to improve consistency and reduce duplicate code. Updated `deleteLevel`
      method to use `number` instead of `string` for the `id` parameter,
      aligning with other methods and ensuring type consistency.

--- backend/src/application/services/levels.service.ts
+++ backend/src/application/services/levels.service.ts
@@ -1,12 +1,9 @@
-import { BadRequestException, HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { ILevelsService } from 'src/domain/interfaces/levels.interface';
 import { Levels } from 'src/domain/models/levels.model';
 import { ILevelsRepository } from '../../domain/repositories/levelsRepository.interface';
-import {
-  PrismaClientKnownRequestError,
-  PrismaClientValidationError,
-} from '@prisma/client/runtime/library';
+import { handleError } from '../../shared/handleError';
 
 @Injectable()
 export class LevelsService implements ILevelsService {
@@ -35,8 +32,8 @@ export class LevelsService implements ILevelsService {
   }
 
   async getLevelById(id: number): Promise<Levels | null> {
-    const level = await this.levelsRepository.getById(id);
     try {
+      const level = await this.levelsRepository.getById(id);
 
       //TODO: find a better way to propagate errors
       if (!level) {
@@ -44,12 +41,7 @@ export class LevelsService implements ILevelsService {
       }
       return level;
     } catch (e) {
-      if (!( e instanceof HttpException )) {
-
-        throw new HttpException(e, HttpStatus.INTERNAL_SERVER_ERROR);
-      }
-
-      throw e;
+handleError(e,  'level')
     }
   }
 
@@ -57,53 +49,33 @@ export class LevelsService implements ILevelsService {
     try {
       return await this.levelsRepository.create(levelData);
     } catch (e) {
-      throw e;
+      handleError(e,  'level')
     }
   }
 
-  async updateLevel(id: number, levelData: Partial<Levels>): Promise<Levels | null> {
+  async updateLevel(id: number, levelData: Prisma.LevelsUpdateInput): Promise<Levels | null> {
     try {
       return await this.levelsRepository.update(id, levelData);
     } catch (e) {
-      throw e;
+      handleError(
+        e,
+        'level');
     }
   }
 
-  async deleteLevel(id: string): Promise<void> {
-    const numberId = Number(id);
-    if (isNaN(numberId) || numberId <= 0) {
-      throw new HttpException('Invalid level ID.', HttpStatus.BAD_REQUEST);
-    }
-
+  async deleteLevel(id: number): Promise<void> {
     try {
-      const levelHasDevs = await this.levelsRepository.checkIfLevelHasDevs(numberId);
+      const levelHasDevs = await this.levelsRepository.checkIfLevelHasDevs(id);
 
       if (levelHasDevs) {
         //TODO: Create a custom exception
-        throw new HttpException('Cannot delete level with associated developers.', HttpStatus.BAD_REQUEST);
-      }
-
-      await this.levelsRepository.delete(numberId);
-    } catch (e) {
-      //TODO: Create a utility function to extract handling
-      if (e instanceof HttpException && e.getStatus() === 400) {
-        throw e //this is too hacky, but will have to do for now
-      }
-
-      if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === 'P2025') { //this means not found
-          throw new HttpException('Level not found.', HttpStatus.NOT_FOUND);
-        }
-      }
-
-      if (e instanceof PrismaClientValidationError) {
-        throw new HttpException('Some parameters are either invalid or missing.',
+        throw new HttpException('Cannot delete level with associated developers.',
           HttpStatus.BAD_REQUEST);
       }
 
-      //TODO: implement a logger method here
-      console.error("Unexpected error: ", e)
-      throw new HttpException('Unexpected error.', HttpStatus.INTERNAL_SERVER_ERROR);
+      await this.levelsRepository.delete(id);
+    } catch (e) {
+      handleError(e, 'level');
     }
   }
 }
--- backend/src/domain/interfaces/levels.interface.ts
+++ backend/src/domain/interfaces/levels.interface.ts
@@ -6,5 +6,5 @@ export interface ILevelsService {
   getLevelById(id: number): Promise<Levels | null>;
   createLevel(levelData: Prisma.LevelsCreateInput): Promise<Levels>;
   updateLevel(id: number, levelData: Prisma.LevelsUpdateInput): Promise<Levels | null>;
-  deleteLevel(id: string): Promise<void>;
+  deleteLevel(id: number): Promise<void>;
 }
