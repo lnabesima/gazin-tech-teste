cbcd9bc3023491fd5a352dcdb25aa07753181730
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-02-03 09:29:31 -0300

    feat(dtos): add UpdateLevelDto for level updates
    Introduce UpdateLevelDto to handle level update requests with validation
    . It includes optional `id` and required `nivel` fields, using
    class-validator and a custom constraint for flexibility. This ensures
    data integrity and proper request validation.

---
+++ backend/src/application/dtos/updateLevel.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { AtLeastOneField } from '../../shared/validators/atLeastOneField.constraint';
+
+export class UpdateLevelDto {
+  @IsOptional()
+  @IsNumber()
+  id?: number;
+
+  @IsString()
+  @IsNotEmpty({message: `The 'nivel' field is required and cannot be empty.`})
+  nivel: string
+}
