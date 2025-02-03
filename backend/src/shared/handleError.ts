e2b1a7e42d3d2eec263dbe64379389620c9ef49c
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-02-03 08:11:15 -0300

    feat: add shared error handling utility
    Introduce a `handleError` function to standardize error handling across
    the application. This includes specific handling for Prisma and HTTP
    exceptions, along with fallback behavior for unexpected errors. A logger
     implementation is noted as a TODO for future enhancement.

---
+++ backend/src/shared/handleError.ts
@@ -0,0 +1,30 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
+
+export function handleError(error: unknown, type: string): never {
+  const normalizedType = type.toLowerCase();
+
+  if (error instanceof HttpException && error.getStatus() === 400) {
+    throw error; //this is too hacky, but will have to do for now
+  }
+
+  if (error instanceof PrismaClientKnownRequestError) {
+    if (error.code === 'P2025') { //this means not found
+      throw new HttpException(`${normalizedType === 'level' ? 'Level' : 'Developer'} not found.`,
+        HttpStatus.NOT_FOUND);
+    }
+  }
+
+  if (error instanceof PrismaClientValidationError) {
+    throw new HttpException('Some parameters are either invalid or missing.',
+      HttpStatus.BAD_REQUEST);
+  }
+
+  //TODO: implement a logger method here
+  console.error('Unexpected error: ', error);
+  throw new HttpException('Unexpected error.', HttpStatus.INTERNAL_SERVER_ERROR);
+}
+
